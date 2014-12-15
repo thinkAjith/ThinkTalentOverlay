fa0c5404a7918ce0f601f8b109f94c247d345abf
thinkAjith <ajith@thinkbridge.in>
2014-12-15 16:09:51 +0530

    First Commit
    First Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thinkTalentOverlayApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the thinkTalentOverlayApp
+ */
+angular.module('thinkTalentOverlayApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
